f20104930b82b4dc210165670b6db9eaa61b6c02
Josimar Camilo <josimarifmg@gmail.com>
2024-02-11 01:06:27 -0300

    adicionando vue-sweetalert2 ao projeto

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import store from "./store";
 import router from "./router";
 import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
+import VueSweetalert2 from 'vue-sweetalert2';
 import ArgonDashboard from "./argon-dashboard";
 
 const appInstance = createApp(App);
 appInstance.use(store);
 appInstance.use(router);
+appInstance.use(VueSweetalert2);
 appInstance.use(ArgonDashboard);
 appInstance.mount("#app");
